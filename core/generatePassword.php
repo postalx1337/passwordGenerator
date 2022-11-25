66167b3b8a44b4e0f949a83acbc1988d06368aeb
postalx1337 <djdf102010@gmail.com>
2022-11-25 14:32:10 +0800

    First Commit

---
+++ core/generatePassword.php
@@ -0,0 +1,28 @@
+<?
+class PasswordGenerator{
+    private $charset = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*";
+    private $length = 0;
+
+    public function setPasswordLength($length){
+        $this->length = $length;
+    }
+
+    public function generatePassword(){
+        if($this->length <= 0){
+            return "Error: please, set password length";
+        }else{
+            $result = '';
+
+            for($i=0; $i<$this->length; $i++){
+                $rndCharacter = $this->charset[mt_rand(0, strlen($this->charset)-1)];
+                $result .= $rndCharacter;
+            }
+            return $result;
+        }
+    }
+}
+
+$pwd = new PasswordGenerator();
+$pwd->setPasswordLength(16);
+echo ($pwd->generatePassword());
+?>
